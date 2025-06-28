0bf6ec4b59a566fd73dc1c15470fc8173911b509
james7132 <contact@jamessliu.com>
2025-06-28 03:06:18 -0700

    Remove obsolete wiki configs

--- towhouwiki-docker-mediawiki/Settings-EVENTS.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once("/var/www/touhouwiki.net/htdocs/CommonSettings.php");
-
-$wgSitename         = "Events Wiki";
-$wgMetaNamespace    = "Events_Wiki";
-$wgLogo             = "$wgScriptPath/skins/ocommon/images/alicelogo.png";
-$wgDBprefix         = "events_";
-$wgLanguageCode = "en";
--- towhouwiki-docker-mediawiki/Settings-MUCK.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once("/var/www/touhouwiki.net/htdocs/CommonSettings.php");
-$wgSitename         = "TouhouMUCK Wiki";
-$wgServer           = "//muck.touhouwiki.net";
-$wgLogo             = "$wgScriptPath/skins/common/images/mucklogo.png";
-$wgDBprefix         = "muck_";
-$wgLanguageCode = "en";
